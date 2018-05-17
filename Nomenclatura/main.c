ca75ba45db3acc683b9cef23a3ae04faf1cd0958
diegost2 <espinozadiego97@hotmail.com>
2018-05-17 10:08:14 -0300

    EmpezandoPunteros

--- Nomenclatura/main.c
+++ Nomenclatura/main.c
@@ -3,6 +3,18 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    //Punteros
+    int *punteroNumero;
+    int numero;
+    numero=66;
+    punteroNumero=&numero;
+    //*punteroNumero=&numero; //No se puede
+    //*punteroNumero=55; //No se puede
+    printf("%d ",numero);
+    printf("\n%p ",&numero);
+    printf("\n%p ",punteroNumero);
+    printf("\n%p ",&punteroNumero);
+    //printf("\n%d ",*punteroNumero);
+
     return 0;
 }
