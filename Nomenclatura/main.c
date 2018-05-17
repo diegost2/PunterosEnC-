3906bd967409889efdb9e230cabffd94f4b9a29a
diegost2 <espinozadiego97@hotmail.com>
2018-05-17 10:25:31 -0300

    pasajes de Punteros

--- Nomenclatura/main.c
+++ Nomenclatura/main.c
@@ -6,15 +6,21 @@ int main()
     //Punteros
     int *punteroNumero;
     int numero;
+    int numeroDos;
     numero=66;
+
     punteroNumero=&numero;
+    numeroDos=&numero;
+    printf("\n(Asigancion de memoria) NumeroDos: %d ",numeroDos); //de memoria
+    numeroDos=*punteroNumero;
+    printf("\n(Asigancion de valor) NumeroDos: %d ",numeroDos);// de valor
     //*punteroNumero=&numero; //No se puede
     //*punteroNumero=55; //No se puede
-    printf("%d ",numero);
-    printf("\n%p ",&numero);
-    printf("\n%p ",punteroNumero);
-    printf("\n%p ",&punteroNumero);
-    //printf("\n%d ",*punteroNumero);
+    printf("\nNumero: %d ",numero);
+    printf("\n&Numero: %p ",&numero);
+    printf("\nPunteroNumero: %p ",punteroNumero);//
+    printf("\n&PunteroNumero: %p ",&punteroNumero);//
+    printf("\n*PunteroNumero: %d ",*punteroNumero);//
 
     return 0;
 }
